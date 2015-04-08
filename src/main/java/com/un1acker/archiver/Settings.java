1359c7729911ed563b951b62fd0d7fd00665d946
un1acker <un1acker@yandex.ru>
2015-04-08 22:54:51 +0400

    change some usage in settings

--- src/main/java/com/un1acker/archiver/Settings.java
+++ src/main/java/com/un1acker/archiver/Settings.java
@@ -10,19 +10,19 @@ import java.util.ArrayList;
  * 02.04.2015
  */
 public class Settings {
-    @Option(name = "-zip", usage = "name for zipFile", forbids = {"-unzip", "-path"})
+    @Option(name = "-zip", usage = "set name for zipFile", forbids = {"-unzip", "-path"})
     private static String zipFileName;
 
-    @Option(name = "-add", usage = "add comment to archiver", depends = {"-zip", "-files"})
+    @Option(name = "-add", usage = "add comment to archiver(exist or non exist)", depends = {"-zip", "-files"})
     private static String addComment;
 
     @Option(name = "-files", usage = "add files to zip", handler = StringArrayOptionHandler.class, depends = "-zip")
     private static ArrayList<String> filesToZip;
 
-    @Option(name = "-unzip", usage = "name for unzipFile", forbids = {"-zip", "-add", "-files"})
+    @Option(name = "-unzip", usage = "set name for unzipFile", forbids = {"-zip", "-add", "-files"})
     private static String unzipFileName;
 
-    @Option(name = "-path", usage = "set path folder to unzip", depends = "-unzip")
+    @Option(name = "-path", usage = "set path folder to unzip(default is folder with name \"zipFileName\" in WORK_DIRECTORY)", depends = "-unzip")
     private static String pathToUnzip;
 
     @Option(name = "-read", usage = "read comment")
